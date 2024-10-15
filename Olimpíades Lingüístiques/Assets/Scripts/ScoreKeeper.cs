0e8f522a76cca988b44025abbca992fefc66c560
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2024-10-15 23:00:33 +0200

    Update ScoreKeeper.cs

--- Olimpíades Lingüístiques/Assets/Scripts/ScoreKeeper.cs
+++ Olimpíades Lingüístiques/Assets/Scripts/ScoreKeeper.cs
@@ -8,7 +8,17 @@ public class ScoreKeeper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< HEAD
 
+=======
+        EnterNewScore("GTS", 50);
+        //EnterNewScore("TET", 550);
+        //EnterNewScore("PTO", 100);
+
+        Debug.Log(scoreList[0].playerName + " " + scoreList[0].playerScore);
+        Debug.Log(scoreList[1].playerName + " " + scoreList[0].playerScore);
+        Debug.Log(scoreList[2].playerName + " " + scoreList[0].playerScore);
+>>>>>>> parent of 8440d14 (scorekeeper finished)
     }
 
     // Update is called once per frame
@@ -23,8 +33,13 @@ public class ScoreKeeper : MonoBehaviour
         {
             if (scoreList[i, 0] != null)
             {
+<<<<<<< HEAD
                 scoreList[i, 0] = name;
                 scoreList[i, 1] = score.ToString();
+=======
+                scoreList[i] = new Score(name, score);
+
+>>>>>>> parent of 8440d14 (scorekeeper finished)
 
                 RearrangeOrder();
 
@@ -33,9 +48,16 @@ public class ScoreKeeper : MonoBehaviour
 
             else if (i == scoreList.Length - 1)
             {
+<<<<<<< HEAD
                 scoreList[i, 0] = name;
                 scoreList[i, 1] = score.ToString();
 
+=======
+                scoreList[i] = new Score(name, score);
+
+
+
+>>>>>>> parent of 8440d14 (scorekeeper finished)
                 RearrangeOrder();
             }
 
@@ -44,8 +66,59 @@ public class ScoreKeeper : MonoBehaviour
 
     private void RearrangeOrder()
     {
+<<<<<<< HEAD
 
     }
+=======
+        //Array.Sort(scoreList[].playerScore);
+        int[] scoreArray = new int[scoreList.Length];
+
+        for (int i = 0; i < scoreList.Length; i++)
+        {
+            if (ReferenceEquals(scoreList[i], null))
+            {
+                //scoreArray[i] = -1;
+            }
+            else
+            {
+                scoreArray[i] = scoreList[i].playerScore;
+                Debug.Log(scoreList[i].playerName + " hemos entrao "+ scoreArray[i]);
+            }
+        }
+
+        Array.Sort(scoreArray);
+
+        string[] nameArray = new string[scoreList.Length];
+
+        for (int i = 0; i < scoreArray.Length; i++)
+        {
+            for (int j = 0; j < scoreList.Length; j++)
+            {
+                if (!ReferenceEquals(scoreList[j], null))
+                {
+                    if (scoreArray[i] == scoreList[j].playerScore)
+                    {
+                        nameArray[i] = scoreList[j].playerName;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < scoreArray.Length; i++)
+        {
+            if (!ReferenceEquals(scoreList[i], null))
+            {
+                scoreList[i] = new Score(nameArray[i], scoreArray[i]);
+            }
+        }
+
+            //fer una array buida i omplirla de tots els numeros
+            //ordenar aquella array
+            //fer una altra array de strings amb el mateix ordre i assignar noms
+            //assignar a la array scoreList el ordre correcte
+
+        }
+>>>>>>> parent of 8440d14 (scorekeeper finished)
 
     private string GetScoreOnPosition(int position)
     {
